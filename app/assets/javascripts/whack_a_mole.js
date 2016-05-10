40f114d2b12595848f90ea18ef68f65f62c86b09
tgaff <tgaff@alumni.nd.edu>
2016-05-10 13:11:24 -0700

    bye remote moles

--- app/assets/javascripts/whack_a_mole.js
+++ app/assets/javascripts/whack_a_mole.js
@@ -1,9 +1,5 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-$(document).on("ajax:success", '.mole', function(e, data) {
-  // gray the image
-  $(this).find('img').toggleClass('grayed');
-  // disable the button (.disabled is provided by bootstrap)
-  $(this).find('input').addClass('disabled');
+$(document).on("what what what", function(e, data) {
 });

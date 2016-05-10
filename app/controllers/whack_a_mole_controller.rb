e1e27b021be8d6fe012aba334ce056f524c29580
tgaff <tgaff@alumni.nd.edu>
2016-05-09 20:03:46 -0700

    add mole model and activerecord

--- app/controllers/whack_a_mole_controller.rb
+++ app/controllers/whack_a_mole_controller.rb
@@ -1,11 +1,7 @@
 class WhackAMoleController < ApplicationController
 
-  MOLES = []
-  1000.times do
-    MOLES.push( {name: FFaker::NameDA.first_name } )
-  end
 
   def index
-    @moles = MOLES
+    @moles = Mole.all
   end
 end
---
+++ app/models/mole.rb
@@ -0,0 +1,2 @@
+class Mole < ActiveRecord::Base
+end

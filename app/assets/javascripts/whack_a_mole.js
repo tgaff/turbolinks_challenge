1c1dadb85c576a3251482b3eb25dc51aa0614a34
tgaff <tgaff@alumni.nd.edu>
2016-05-09 21:30:42 -0700

    simplify solution to work with/without turbolinks

--- app/assets/javascripts/whack_a_mole.js
+++ app/assets/javascripts/whack_a_mole.js
@@ -1,12 +1,9 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-$(document).on('ready', function() {
-  $('.mole').on("ajax:success", function(e, data) {
-    // gray the image
-    $(this).find('img').toggleClass('grayed');
-    // disable the button (.disabled is provided by bootstrap)
-    $(this).find('input').addClass('disabled');
-  });
-
-})
+$(document).on("ajax:success", '.mole', function(e, data) {
+  // gray the image
+  $(this).find('img').toggleClass('grayed');
+  // disable the button (.disabled is provided by bootstrap)
+  $(this).find('input').addClass('disabled');
+});

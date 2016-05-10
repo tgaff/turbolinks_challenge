cd9442899fa73bfaa1b070af8f578ab44131b077
tgaff <tgaff@alumni.nd.edu>
2016-05-09 21:04:46 -0700

    solution +

--- app/assets/javascripts/whack_a_mole.js
+++ app/assets/javascripts/whack_a_mole.js
@@ -1,2 +1,12 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).on('ready', function() {
+  $('.mole').on("ajax:success", function(e, data) {
+    // gray the image
+    $(this).find('img').toggleClass('grayed');
+    // disable the button (.disabled is provided by bootstrap)
+    $(this).find('input').addClass('disabled');
+  });
+
+})
